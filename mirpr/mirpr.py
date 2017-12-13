9727001b3e8c0c04705b1501bea21608b34ce30d
wilversings <aiordachioaei.marius@gmail.com>
2017-12-13 10:49:53 +0200

    .

--- mirpr/mirpr.py
+++ mirpr/mirpr.py
@@ -10,15 +10,15 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 cells = [np.hsplit(row, 100) for row in np.vsplit(gray, 50)]
 
-flatten = reduce(lambda a, b: a + b, cells)
+flat = reduce(lambda a, b: a + b, cells)
 
-knn = MatrixKnn(flatten, None)
+knn = MatrixKnn(flat, None)
 
 test_im = np.ndarray.astype(cv2.cvtColor(cv2.imread('test.png'), cv2.COLOR_BGR2GRAY), 'int64')
 
 number, indexes, knn_hash = knn.get_thresholded_match(test_im)
 
 print("Guessed digit: " + str(number));
-print("With a score of: " + "{:,}".format((
-    sum(map(lambda x : MatrixKnn.euclidian_squared(test_im, x), flatten[number * 500: (number + 1) * 500]))
-)))
+print("With a score of: " + "{:,}".format(
+    sum(map(lambda x : MatrixKnn.euclidian_squared(test_im, x), flat[number * 500: (number + 1) * 500]))
+))
